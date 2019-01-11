ab9b039f65c8b903d1db2706ddc8e1d4994a9387
Anna Shkulova <>
2019-01-11 15:55:05 +0200

    Refactoring ReqSpecification class

--- src/test/java/spec/ReqSpecification.java
+++ src/test/java/spec/ReqSpecification.java
@@ -4,31 +4,27 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
 
 
 public class ReqSpecification {
-    private static Properties properties = loadPropertiesFile("app.properties");
+    private static ResourceBundle propertiesFile;
+
+    static {
+        try {
+            propertiesFile = new PropertyResourceBundle(ReqSpecification.class.getClassLoader()
+                    .getResourceAsStream("app.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static final RequestSpecification reqSpec = new RequestSpecBuilder()
             .setContentType(ContentType.JSON)
-            .setBaseUri(properties.getProperty("url"))
-            .setPort(Integer.parseInt(properties.getProperty("port")))
-            .setBasePath(properties.getProperty("endpoint"))
+            .setBaseUri(propertiesFile.getString("url"))
+            .setPort(Integer.parseInt(propertiesFile.getString("port")))
+            .setBasePath(propertiesFile.getString("endpoint"))
             .build();
 
-    private static Properties loadPropertiesFile(String filePath) {
-
-        Properties prop = new Properties();
-
-        try (InputStream resourceAsStream = ReqSpecification.class.getClassLoader().getResourceAsStream(filePath)) {
-            prop.load(resourceAsStream);
-        } catch (IOException e) {
-            System.err.println("Unable to load properties file : " + filePath);
-        }
-
-        return prop;
-
-    }
 }
